b3763bb529be8d5ac3f9f1a9b9dba60f78e931d1
Jacky <zzlyzq@users.noreply.github.com>
2017-04-18 20:25:31 +0800

    对于新的vmware机器，启动并停止可以达到既获得mac地址分配也能为dhcp配置争取点时间。
      #启动并且停止
      vim-cmd vmsvc/power.on $vmid
      sleep 2
      vim-cmd vmsvc/power.off $vmid

--- step3/dhcp.sh
+++ step3/dhcp.sh
@@ -7,6 +7,11 @@ do
   vmname=`echo $line | awk '{print $2}'`
   vmip=`echo $vmname | awk -F\- '{print $NF}'`
   vmip=`echo $vmip | awk -FP '{print "192.168.0."$NF}'`
+  #启动并且停止
+  vim-cmd vmsvc/power.on $vmid
+  sleep 2
+  vim-cmd vmsvc/power.off $vmid
+  # 采集mac地址
   vmmac=`vim-cmd vmsvc/device.getdevices $vmid | grep macAddress | awk -F\" '{print $2}'`
   echo "$vmid $vmname $vmip $vmmac"
   echo "dhcp-host=$vmmac,$vmip" >> /tmp/dhcp.txt
